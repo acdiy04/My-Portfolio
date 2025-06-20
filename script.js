2610d18f4c6e2b58972801f9769a1ce2455940d3
acdiy04 <acdiy.a.01@gmail.com>
2025-06-20 18:39:13 +0530

    initial upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Collapsible sections
+    const coll = document.querySelectorAll(".collapsible");
+    coll.forEach(btn => {
+      btn.addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    });
+  });
+  
